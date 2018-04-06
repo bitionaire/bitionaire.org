028a59f7867c7ca7d772923d44262fb0aa3f940d
bitionaire <johann.boehler@googlemail.com>
2018-04-06 19:15:50 +0200

    Added logo stuff to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react';
 import './Footer.css';
 
+const Logo = (path: string) => {
+  let content = require(`!svg-inline-loader!./assets/${path}.svg`);
+  return (
+    <span
+      className="footer__contact-brand-logo"
+      dangerouslySetInnerHTML={{__html: content}}
+    />
+  );
+};
+
 const Footer: React.SFC = () => (
   <footer className="footer">
 
@@ -9,18 +19,21 @@ const Footer: React.SFC = () => (
 
     <ul className="footer__contact-list">
       <li className="footer__contact-list-item">
-        <a href="twitter">
-          <span>Twitter</span>
+        <a href="twitter" target="_blank" className="footer__brand-link">
+          {Logo('twitter')}
+          <span className="footer__contact-brand-name">Twitter</span>
         </a>
       </li>
       <li className="footer__contact-list-item">
-        <a href="github">
-          <span>GitHub</span>
+        <a href="github" target="_blank" className="footer__brand-link">
+          {Logo('github')}
+          <span className="footer__contact-brand-name">GitHub</span>
         </a>
       </li>
       <li className="footer__contact-list-item">
-        <a href="linkedin">
-          <span>LinkedIn</span>
+        <a href="linkedin" target="_blank" className="footer__brand-link">
+          {Logo('linkedin')}
+          <span className="footer__contact-brand-name">LinkedIn</span>
         </a>
       </li>
     </ul>
