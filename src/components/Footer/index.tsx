48cb6e8ed86a4bb3f47cd9191a8fde065c80ac76
bitionaire <johann.boehler@googlemail.com>
2018-04-06 21:11:42 +0200

    Added footer styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import './Footer.css';
 
 const Logo = (path: string) => {
-  let content = require(`!svg-inline-loader!./assets/${path}.svg`);
+  let content = require(`./assets/${path}.svg`);
   return (
-    <span
+    <img
       className="footer__contact-brand-logo"
-      dangerouslySetInnerHTML={{__html: content}}
+      src={content}
     />
   );
 };
@@ -14,24 +14,24 @@ const Logo = (path: string) => {
 const Footer: React.SFC = () => (
   <footer className="footer">
 
-    <h1>Johann Böhler</h1>
-    <a href="mailto:info@bitionaire.org">info@bitionaire.org</a>
+    <h1 className="footer__name">Johann Böhler</h1>
+    <a href="mailto:info@bitionaire.org" className="footer__mail">info@bitionaire.org</a>
 
     <ul className="footer__contact-list">
       <li className="footer__contact-list-item">
-        <a href="twitter" target="_blank" className="footer__brand-link">
+        <a href="https://twitter.com/thebitionaire" target="_blank" className="footer__brand-link">
           {Logo('twitter')}
           <span className="footer__contact-brand-name">Twitter</span>
         </a>
       </li>
       <li className="footer__contact-list-item">
-        <a href="github" target="_blank" className="footer__brand-link">
+        <a href="https://github.com/bitionaire" target="_blank" className="footer__brand-link">
           {Logo('github')}
           <span className="footer__contact-brand-name">GitHub</span>
         </a>
       </li>
       <li className="footer__contact-list-item">
-        <a href="linkedin" target="_blank" className="footer__brand-link">
+        <a href="https://de.linkedin.com/in/johann-b%C3%B6hler-279191a3" target="_blank" className="footer__brand-link">
           {Logo('linkedin')}
           <span className="footer__contact-brand-name">LinkedIn</span>
         </a>
